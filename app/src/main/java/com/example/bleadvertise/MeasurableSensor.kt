894f4c48ecee728604b333bd0c3d0e339b295041
MAzharAR03 <1221100800@student.mmu.edu.my>
2025-11-28 13:16:09 +0800

    added accelerometer and step sensors + bluetooth characteristics

---
+++ app/src/main/java/com/example/bleadvertise/MeasurableSensor.kt
@@ -0,0 +1,18 @@
+package com.example.sensorguide
+//following Tutorial:https://www.youtube.com/watch?v=IU-EAtITRRM
+abstract class MeasurableSensor (
+    protected val sensorType: Int
+) {
+    protected var onSensorValuesChanged: ((List<Float>) -> Unit)? = null
+
+    abstract val doesSensorExist: Boolean
+
+    abstract fun startListening()
+    abstract fun stopListening()
+
+    fun setOnSensorValuesChangedListener(listener: (List<Float>) -> Unit){
+        onSensorValuesChanged = listener
+    }
+
+
+}
